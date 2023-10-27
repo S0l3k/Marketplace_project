6ad36cbb49aeedb8d961e0edefceed1a62d078e4
S0l3k <turtleteamwcg@gmail.com>
2023-10-27 11:34:53 +0300

    fix: html templates work

--- marketplace_root/marketplace/settings.py
+++ marketplace_root/marketplace/settings.py
@@ -118,13 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "marketplace/static/",
+    os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
